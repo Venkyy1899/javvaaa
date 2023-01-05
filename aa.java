b2d6ffcb716c8316bfab9b1f548f0a41945bc484
Venkyy1899 <113653963+Venkyy1899@users.noreply.github.com>
2023-01-05 14:17:35 +0530

    Create aa.java

---
+++ aa.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class aa {
+       public static void main(String[] args)
+	throws IOException
+	{
+	String str ="i";
+                    Scanner sc=new Scanner(System.in);
+                    str=sc.nextLine();
+ System.out.println("Total no of vowels in string are: " );
+}
+}
