9ed75ed1ab065a336376a6a039b284fa2316503a
Venkyy1899 <113653963+Venkyy1899@users.noreply.github.com>
2023-01-05 14:20:46 +0530

    Create char.java

---
+++ char.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class chat
+{
+public static void main(String args [])
+{
+char a;
+System.out.println("Enter a character= ");
+Scanner sc =new Scanner(System.in);
+a=sc.next().charAt(0);
+while(a=char)
+System.out.println("you entered " +a);
+count++;
+}
+}
