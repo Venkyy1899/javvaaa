dd7995b0935e4698d85e3ee0be0edc6b72e01ec3
Venkyy1899 <113653963+Venkyy1899@users.noreply.github.com>
2023-01-05 14:23:24 +0530

    Create rev.java

---
+++ rev.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class rev {
+	public static void main(String[] args)
+	{
+                   
+		Integer array[] = { 1, 2, 3, 4, 5 };
+		Arrays.sort(array);
+		System.out.println(Arrays.(array));
+	}
+}
