25b18219be1d94e2b5c9b9b6c14431cb167d6b94
Venkyy1899 <113653963+Venkyy1899@users.noreply.github.com>
2023-01-05 14:18:21 +0530

    Create add.java

---
+++ add.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class add
+{
+public static void main(String [] args)
+{
+int a,b,c;
+System.out.println("Enter two numbers");
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter a number= ");
+a=sc.nextInt();
+System.out.println("Enter another number= ");
+b=sc.nextInt();
+System.out.println("The sum of 2 numbers= " +(a+b) +"\n" +"Multiplication of 2 numbers is =" +(a*b));
+}
+}
